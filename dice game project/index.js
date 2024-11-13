44c033fad52f944f2e0a8dc1d14370f4431f80a0
Sean-Pend <pendgaming1@gmail.com>
2024-11-13 14:29:13 +1000

    Add files via upload

---
+++ dice game project/index.js
@@ -0,0 +1,23 @@
+// Player 1 dice roll
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+// Player 2 dice roll
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource = "images/" + randomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource);
+// Change title to display winner
+// Hint: You'll need to use an if statement, querySelector() 
+// and the innerHTML property to change the text in the h1.
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Its a draw";
+}
